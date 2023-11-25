15f01f2243492916efe69f83a4179d7a850fbed6
a-t-0 <you@example.com>
2023-11-25 20:45:51 +0100

    Included path.

--- src/main.sh
+++ src/main.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+echo "$PWD"
+
 # shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/installation/install_apt.sh"
+# shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/installation/install_pip.sh"
+# shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/installation/install_snap.sh"
 
+# shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/logging/cli_logging.sh"
 
+# shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/uninstallation/uninstall_apt.sh"
+# shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/uninstallation/uninstall_pip.sh"
+# shellcheck disable=SC1091
 source "$(dirname "${BASH_SOURCE[0]}")/uninstallation/uninstall_snap.sh"
+
+ensure_pip_pkg "twine"
+pip_remove "twine"
