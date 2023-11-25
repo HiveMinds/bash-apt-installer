d9ed0224362fe2808955edaaa2f012ecfd05ddfb
a-t-0 <you@example.com>
2023-11-25 20:50:25 +0100

    Made imports relative to path of script.

--- src/main.sh
+++ src/main.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-echo "$PWD"
+
+# Get the path towards this src dir.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/installation/install_apt.sh"
+source "$SCRIPT_DIR/installation/install_apt.sh"
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/installation/install_pip.sh"
+source "$SCRIPT_DIR/installation/install_pip.sh"
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/installation/install_snap.sh"
+source "$SCRIPT_DIR/installation/install_snap.sh"
 
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/logging/cli_logging.sh"
+source "$SCRIPT_DIR/logging/cli_logging.sh"
 
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/uninstallation/uninstall_apt.sh"
+source "$SCRIPT_DIR/uninstallation/uninstall_apt.sh"
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/uninstallation/uninstall_pip.sh"
+source "$SCRIPT_DIR/uninstallation/uninstall_pip.sh"
 # shellcheck disable=SC1091
-source "$(dirname "${BASH_SOURCE[0]}")/uninstallation/uninstall_snap.sh"
-
-ensure_pip_pkg "twine"
-pip_remove "twine"
+source "$SCRIPT_DIR/uninstallation/uninstall_snap.sh"
