7c44645365b6e5416b1cba50d7de6337c3d67911
a-t-0 <you@example.com>
2023-11-26 01:44:06 +0100

    Changed SCRIPT_DIR global to local.

--- src/main.sh
+++ src/main.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-# Load the bash-log dependency.
-source dependencies/bash-log/src/main.sh
+source "dependencies/bash-log/src/main.sh"
 
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/parsing_helper.sh"
+function load_functions() {
+  local script_dir
+  script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Get the path towards this src dir.
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  # shellcheck disable=SC1091
+  source "$script_dir/parsing_helper.sh"
 
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/installation/install_apt.sh"
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/installation/install_pip.sh"
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/installation/install_snap.sh"
+  # Get the path towards this src dir.
+  # script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/uninstallation/uninstall_apt.sh"
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/uninstallation/uninstall_pip.sh"
-# shellcheck disable=SC1091
-source "$SCRIPT_DIR/uninstallation/uninstall_snap.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/installation/install_apt.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/installation/install_pip.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/installation/install_snap.sh"
+
+  # shellcheck disable=SC1091
+  source "$script_dir/uninstallation/uninstall_apt.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/uninstallation/uninstall_pip.sh"
+  # shellcheck disable=SC1091
+  source "$script_dir/uninstallation/uninstall_snap.sh"
+}
+load_functions
