4a5bd9df949ed38d2b4737f02aca79c64a1221e6
a-t-0 <you@example.com>
2023-11-26 01:14:54 +0100

    Corrected typo.

--- src/main.sh
+++ src/main.sh
@@ -4,7 +4,7 @@
 source dependencies/bash-log/src/main.sh
 
 # shellcheck disable=SC1091
-source "$SCRIPT_DIR/src/parsing_helper.sh"
+source "$SCRIPT_DIR/parsing_helper.sh"
 
 # Get the path towards this src dir.
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
