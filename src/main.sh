e3e65609e8be27e0ce86d062dde1743468344494
a-t-0 <you@example.com>
2023-11-26 20:19:02 +0100

    Updated usage instructions, prevented load main.sh from running anything.

--- src/main.sh
+++ src/main.sh
@@ -9,9 +9,6 @@ function load_functions() {
   # shellcheck disable=SC1091
   source "$script_dir/parsing_helper.sh"
 
-  # Get the path towards this src dir.
-  # script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
   # shellcheck disable=SC1091
   source "$script_dir/installation/install_apt.sh"
   # shellcheck disable=SC1091
